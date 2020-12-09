5bc1bf54860b684c4eaae8416255bfb0db1004a3
quazlee <50329229+quazlee@users.noreply.github.com>
2020-12-08 20:57:15 -0500

    Add files via upload

--- serverHack.js
+++ serverHack.js
@@ -4,11 +4,14 @@ export async function main(ns) {
   const MONEY_PERCENT = 0.8; //The percent of a server's max money the target has to be above in order to hack
   const SECURITY_PERCENT = 0.5; //The percent of a server's security (btwn min + base) the target has to be above to weaken
   
-  const securityThreshold = (ns.getServerBaseSecurityLevel(target) - ns.getServerMinSecurityLevel(target))*SECURITY_PERCENT + ns.getServerMinSecurityLevel(target);
+  const minSecurity = ns.getServerMinSecurityLevel(target)
+  const securityThreshold = (ns.getServerBaseSecurityLevel(target) - minSecurity)*SECURITY_PERCENT + minSecurity;
   
   while (true) {
   	if (ns.getServerSecurityLevel(target) > securityThreshold) {
-    	await ns.weaken(target);
+    	do {
+				await ns.weaken(target);
+			} while (ns.getServerSecurityLevel(target) > minSecurity + 0.2);     
     }else if (ns.getServerMoneyAvailable(target) > (ns.getServerMaxMoney(target))*MONEY_PERCENT){
     	await ns.hack(target);
     } else {
