2a5a0e18b8bd0cdd258cfb9d5d1fa449fa720f3b
quazlee <50329229+quazlee@users.noreply.github.com>
2020-12-09 15:18:31 -0500

    Update simpleDeploy.js

--- simpleDeploy.js
+++ simpleDeploy.js
@@ -4,6 +4,9 @@ export async function main(ns) {
     let scriptTarget = ns.args[2];
     let serverRam = ns.getServerRam(deployTarget);
     let scriptRam = ns.getScriptRam(script);
-    let threads = Math.floor((serverRam[0]-serverRam[1])/scriptRam);
+    let threads = Math.floor((serverRam[0] - serverRam[1]) / scriptRam);
+    if (!ns.fileExists(script, deployTarget)) {
+        ns.scp(script, "home", deployTarget);
+    }
     ns.exec(script, deployTarget, threads, scriptTarget);
 }
