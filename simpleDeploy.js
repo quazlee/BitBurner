6c5e761e5f4ce8481cd6ec20b8595ae8efa6cc52
quazlee <50329229+quazlee@users.noreply.github.com>
2020-12-09 14:23:40 -0500

    Add files via upload

---
+++ simpleDeploy.js
@@ -0,0 +1,9 @@
+export async function main(ns) {
+    let script = ns.args[0];
+    let deployTarget = ns.args[1];
+    let scriptTarget = ns.args[2];
+    let serverRam = ns.getServerRam(deployTarget);
+    let scriptRam = ns.getScriptRam(script);
+    let threads = Math.floor((serverRam[0]-serverRam[1])/scriptRam);
+    ns.exec(script, deployTarget, threads, scriptTarget);
+}
