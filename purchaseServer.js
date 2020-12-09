d515249107e05eafc99b16a99d8934929bb6c3f3
quazlee <50329229+quazlee@users.noreply.github.com>
2020-12-08 20:17:51 -0500

    Add files via upload

---
+++ purchaseServer.js
@@ -0,0 +1,20 @@
+export async function main(ns) {
+    let price = ns.getPurchasedServerCost(ns.args[0]);
+    let number = ns.args[1];
+    let purchase = await ns.prompt("Would you like to purchase " + ns.args[1] + ns.args[0] + "gb server for $" + price*number + "? You currently have $" + ns.getServerMoneyAvailable('home') + ".");
+    if (purchase === true) {
+        let nameBase = 'Server';
+        let nameModifier = Math.floor(Math.random() * 1000000) + 100000;
+        let serverName = nameBase.concat("|", ns.args[0], "|", nameModifier);
+        if (ns.getServerMoneyAvailable('home') >= price) {
+            ns.purchaseServer(serverName, args[0]);
+            ns.tprint("s")
+        } else {
+            ns.tprint("You do not have the funds to purchase the specified size server.");
+        }
+    } else{
+        ns.tprint("You did not purchase any servers.")
+    }
+
+
+}
