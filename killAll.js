f8e58f04355a61f6b1a69099d933280be17d7ada
quazlee <50329229+quazlee@users.noreply.github.com>
2020-12-09 17:48:40 -0500

    Add files via upload

---
+++ killAll.js
@@ -0,0 +1,50 @@
+export async function main(ns) {
+    let scanList;
+    let mapList;
+    let numPortsOpen = 0;
+    let counter = 0;
+    scanList = [];
+    buildScanList(scanList, "home", "", ns)
+    scanList.splice(0, 1);
+    ns.tprint(scanList);
+
+    if (ns.fileExists('BruteSSH.exe')) {
+        scanList.map(x => ns.brutessh(x));
+        numPortsOpen++;
+    }
+
+    if (ns.fileExists('FTPCrack.exe')) {
+        scanList.map(x => ns.ftpcrack(x));
+        numPortsOpen++;
+    }
+
+    if (ns.fileExists('relaySMTP.exe')) {
+        scanList.map(x => ns.relaysmtp(x));
+        numPortsOpen++;
+    }
+
+    if (ns.fileExists('HTTPWorm.exe')) {
+        scanList.map(x => ns.httpworm(x));
+        numPortsOpen++;
+    }
+
+    if (ns.fileExists('SQLInject.exe')) {
+        scanList.map(x => ns.sqlinject(x));
+        numPortsOpen++;
+    }
+
+
+    scanList.map(x => ns.killall(x));
+
+}
+
+function buildScanList(scanList, node, parent, ns) {
+
+    let children = ns.scan(node);
+    scanList.push(node);
+
+
+    for (let i = 0; i < children.length; i++) {
+        if (children[i] != parent) buildScanList(scanList, children[i], node, ns);
+    }
+}
