963fcc3e58ba528311a74ef9b9afc178d25880c8
quazlee <50329229+quazlee@users.noreply.github.com>
2020-12-12 13:35:43 -0500

    Add files via upload

--- brainMain.js
+++ brainMain.js
@@ -1,7 +1,7 @@
 export async function main(ns){
     var target = ns.args[0];
     var host;
-    var scanList = ns.scan;
+    var scanList = ns.scan();
     var SECURITY_MODIFIER = .4;
     var MONEY_MODIFIER = .5;
     scanList = getServers(scanList, "Server", ns);
@@ -9,49 +9,67 @@ export async function main(ns){
     while(true){
         if(((ns.getServerBaseSecurityLevel(target) - ns.getServerMinSecurityLevel(target)) * SECURITY_MODIFIER) + ns.getServerMinSecurityLevel(target)){
             scanList.map(x => ns.killall(x));
-            serverWeaken();
+            let script = "brainHack.js";
+            let serverRam = ns.getServerRam(target);
+            let scriptRam = ns.getScriptRam(script);
+            let threads = (((serverRam[0] - serverRam[1])) / scriptRam) - 1;
+            scanlist.map(x => ns.exec(script, x, threads, target));
         } else if(ns.getServerMoneyAvailable(target) < ns.getServerMaxMoney(target) * MONEY_MODIFIER){
             scanList.map(x => ns.killall(x));
-            serverGrow();
+            let script = "brainGrow.js";
+            let serverRam = ns.getServerRam(target);
+            let scriptRam = ns.getScriptRam(script);
+            let threads = (((serverRam[0] - serverRam[1])) / scriptRam) - 1;
+            scanlist.map(x => ns.exec(script, x, threads, target));
         } else{
             scanList.map(x => ns.killall(x));
-            serverHack();
-        }
+            let script = "brainHack.js";
+            let scriptRam = ns.getScriptRam(script);
+            let threads = ns.hackAnalyzeThreads(target);
+            let serverSpace = (((serverRam[0] - serverRam[1])) / scriptRam) - 1;
+            scanList.map(function(x){
+                if (serverSpace > threads){
+                    ns.exec(script, x, threads, target);
+                }else{
+                    ns.exec(script, x, serverSpace, target);
+                }
+            })
+                }
     }
     
 }
 
-function serverGrow(target, host, ns){
-    let script = "brainGrow.js";
-    let serverRam = ns.getServerRam(target);
-    let scriptRam = ns.getScriptRam(script);
-    let threads = (((serverRam[0] - serverRam[1])) / scriptRam) - 1;
-    scanlist.map(x => ns.exec(script, x, threads, target));
-}
+// function serverGrow(target, host, ns){
+//     let script = "brainGrow.js";
+//     let serverRam = ns.getServerRam(target);
+//     let scriptRam = ns.getScriptRam(script);
+//     let threads = (((serverRam[0] - serverRam[1])) / scriptRam) - 1;
+//     scanlist.map(x => ns.exec(script, x, threads, target));
+// }
 
-function serverWeaken(ram, ns){
-    let script = "brainHack.js";
-    let serverRam = ns.getServerRam(target);
-    let scriptRam = ns.getScriptRam(script);
-    let threads = (((serverRam[0] - serverRam[1])) / scriptRam) - 1;
-    scanlist.map(x => ns.exec(script, x, threads, target));
-}
+// function serverWeaken(ram, ns){
+//     let script = "brainHack.js";
+//     let serverRam = ns.getServerRam(target);
+//     let scriptRam = ns.getScriptRam(script);
+//     let threads = (((serverRam[0] - serverRam[1])) / scriptRam) - 1;
+//     scanlist.map(x => ns.exec(script, x, threads, target));
+// }
 
-//deploys hacking scripts that obtain a percentage of the total money
-function serverHack(ns){
-    let script = "brainHack.js";
-    let scriptRam = ns.getScriptRam(script);
-    let threads = ns.hackAnalyzeThreads(target);
-    let serverSpace = (((serverRam[0] - serverRam[1])) / scriptRam) - 1;
-    scanList.map(function(x){
-        if (serverSpace > threads){
-            ns.exec(script, x, threads, target);
-        }else{
-            ns.exec(script, x, serverSpace, target);
-        }
-    })
+// //deploys hacking scripts that obtain a percentage of the total money
+// function serverHack(ns){
+//     let script = "brainHack.js";
+//     let scriptRam = ns.getScriptRam(script);
+//     let threads = ns.hackAnalyzeThreads(target);
+//     let serverSpace = (((serverRam[0] - serverRam[1])) / scriptRam) - 1;
+//     scanList.map(function(x){
+//         if (serverSpace > threads){
+//             ns.exec(script, x, threads, target);
+//         }else{
+//             ns.exec(script, x, serverSpace, target);
+//         }
+//     })
 
-}
+// }
 
 // returns array of player owned servers, excluding home
 function getServers(arr, query, ns){
