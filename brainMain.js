7e61dd04b8db969c9cc5dca58c5fcb455df81f30
quazlee <50329229+quazlee@users.noreply.github.com>
2020-12-12 13:03:30 -0500

    Update brainMain.js

--- brainMain.js
+++ brainMain.js
@@ -1,32 +1,42 @@
 export async function main(ns){
     var target;
     var host;
-    var hackThreads;
-    var growThreads;
-    var weakenThreads;
-    var scanList;
+    var scanList = ns.scan;
 
-    getServers(scanList, Server, ns);
+    scanList = getServers(scanList, "Server", ns);
+    ns.tprint(scanList);
 
-    ns.print(getServers(scanList, Server, ns));
+    // if((ns.getServerBaseSecurityLevel(target) - ns.getServerMinSecurityLevel(target))*.3 + ns.getServerMinSecurityLevel(target)){
+
+    //     serverWeaken();
+    // }
 
     
 }
 
-function serverGrow(ram, ns){
-    growThreads = (((ram[0] - serverRam[1])) / scriptRam) - 1;
-}
+// function serverGrow(target, host, ns){
+//     let script = "brainGrow.js";
+//     let serverRam = ns.getServerRam(target);
+//     let scriptRam = ns.getScriptRam(script);
+//     let threads = (((serverRam[0] - serverRam[1])) / scriptRam) - 1;
+//     ns.exec(script, host, threads, target);
+// }
 
-function serverWeaken(ram, ns){
-    weakenThreads = (((ram[0] - serverRam[1])) / scriptRam) - 1;
-}
+// function serverWeaken(ram, ns){
+//     let script = "brainHack.js";
+//     let serverRam = ns.getServerRam(target);
+//     let scriptRam = ns.getScriptRam(script);
+//     let threads = (((serverRam[0] - serverRam[1])) / scriptRam) - 1;
+// }
 
-function serverHack(ns){
+// //deploys hacking scripts that obtain a percentage of the total money
+// function serverHack(ns){
+//     let script = "brainHack.js";
 
-}
+// }
 
+//returns array of player owned servers, excluding home
 function getServers(arr, query, ns){
-    arr = ns.scan();
     return arr.filter(function(el) {
         return el.toLowerCase().indexOf(query.toLowerCase()) !== -1;
     })
