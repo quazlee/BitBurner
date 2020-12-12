a2af940b5c52ae5f12cd5dfd0e889a3a15a0d3d2
quazlee <50329229+quazlee@users.noreply.github.com>
2020-12-12 13:47:31 -0500

    Add files via upload

--- brainMain.js
+++ brainMain.js
@@ -11,12 +11,11 @@ export async function main(ns){
             for(let i = 0; 0 < scanList.length; i++){
                 ns.killall(scanList[i]);
             }
-            scanList.map(x => ns.killall(x));
+
             let script = "brainHack.js";
             let serverRam = ns.getServerRam(target);
             let scriptRam = ns.getScriptRam(script);
             let threads = (((serverRam[0] - serverRam[1])) / scriptRam) - 1;
-            scanList.map(x => ns.exec(script, x, threads, target));
             for(let i = 0; 0 < scanList.length; i++){
                 ns.exec(script, scanList[i], threads, target);
             }
@@ -52,43 +51,7 @@ export async function main(ns){
     
 }
 
-for(let i = 0; 0 < scanList.length; i++){
 
-}
-
-// function serverGrow(target, host, ns){
-//     let script = "brainGrow.js";
-//     let serverRam = ns.getServerRam(target);
-//     let scriptRam = ns.getScriptRam(script);
-//     let threads = (((serverRam[0] - serverRam[1])) / scriptRam) - 1;
-//     scanlist.map(x => ns.exec(script, x, threads, target));
-// }
-
-// function serverWeaken(ram, ns){
-//     let script = "brainHack.js";
-//     let serverRam = ns.getServerRam(target);
-//     let scriptRam = ns.getScriptRam(script);
-//     let threads = (((serverRam[0] - serverRam[1])) / scriptRam) - 1;
-//     scanlist.map(x => ns.exec(script, x, threads, target));
-// }
-
-// //deploys hacking scripts that obtain a percentage of the total money
-// function serverHack(ns){
-//     let script = "brainHack.js";
-//     let scriptRam = ns.getScriptRam(script);
-//     let threads = ns.hackAnalyzeThreads(target);
-//     let serverSpace = (((serverRam[0] - serverRam[1])) / scriptRam) - 1;
-//     scanList.map(function(x){
-//         if (serverSpace > threads){
-//             ns.exec(script, x, threads, target);
-//         }else{
-//             ns.exec(script, x, serverSpace, target);
-//         }
-//     })
-
-// }
-
-// returns array of player owned servers, excluding home
 function getServers(arr, query, ns){
     return arr.filter(function(el) {
         return el.toLowerCase().indexOf(query.toLowerCase()) !== -1;
